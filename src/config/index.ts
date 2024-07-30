ba9aff5dd8f59e0bdd8d1ed8742306fffbb6216b
feyyazakkus <feyyazakkus@gmail.com>
2024-07-30 16:13:02 +0200

    updated config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,21 @@
-const config = {
-    dynamoDB: {
-        region: "eu-central-1",
-        endpoint: 'http://localhost:8000',
+const env: string = process.env.NODE_ENV || "dev";
+
+interface IConfig {
+    [key: string]: any
+  }
+
+const config: IConfig = {
+    "dev": {
+        dynamoDB: {
+            region: "eu-central-1",
+            endpoint: 'http://localhost:8000',
+        }
+    },
+    "prod": {
+        dynamoDB: {
+            region: "eu-central-1"
+        }
     }
 };
 
-export default config;
+export default config[env];
